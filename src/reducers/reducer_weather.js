4fda2ff5c8f0bfa013e976c9720468fc155f12ab
fangjian <fangjian@mokapos.com>
2018-01-02 10:02:56 +0000

    Section 5, Lecture 64

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,10 @@
-export default function (state = null, action) {
-    console.log(action);
+import {FETCH_WEATHER} from "../actions";
+
+export default function (state = [], action) {
+    switch (action.type) {
+        case FETCH_WEATHER: {
+            return [action.payload.data, ...state];
+        }
+    }
     return state;
 }
