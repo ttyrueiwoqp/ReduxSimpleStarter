497086eb308ac3e5f0619be0dfbb9e0d1767feab
fangjian <fangjian@mokapos.com>
2018-01-02 08:48:29 +0000

    Section 5, Lecture 59

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_KEY = '1c87f00611ed80cbbb83ef081f3d1b04';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
